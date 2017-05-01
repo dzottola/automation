0be883d9ea10a6b33b824027e091848a039b91a7
dzottola <dzottola>
2017-05-01 18:13:26 -0300

    Push ProjectAutomation

---
+++ src/main/java/com/mavenproject/LoadTest01.java
@@ -0,0 +1,50 @@
+package com.mavenproject;
+ 
+import java.util.concurrent.TimeUnit;
+//--
+import org.openqa.selenium.By;
+//--
+import org.openqa.selenium.WebDriver;
+//--
+import org.openqa.selenium.WebElement;
+//--
+import org.openqa.selenium.firefox.FirefoxDriver;
+//--
+import org.testng.Assert;
+//--
+import org.testng.annotations.AfterClass;
+//--
+import org.testng.annotations.BeforeClass;
+//--
+import org.testng.annotations.Test;
+ 
+public class LoadTest01 {
+ 
+    private WebDriver driver;
+ 
+    @BeforeClass
+    public void beforeClass() {
+        driver = new FirefoxDriver();
+    }
+ 
+//    @AfterClass
+//    public void afterClass() {
+//        driver.quit();
+//    }
+ 
+    @Test
+    public void verifySearchButton() {
+ 
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        System.out.println("starting est");
+
+        driver.get("http://www.google.com");
+        String search_text = "Buscar  Google";
+        WebElement search_button = driver.findElement(By.name("btnK"));
+ 
+        String text = search_button.getAttribute("value");
+        System.out.println("closing est");
+
+        Assert.assertEquals(text, search_text, "Text not found!");
+    }
+}
